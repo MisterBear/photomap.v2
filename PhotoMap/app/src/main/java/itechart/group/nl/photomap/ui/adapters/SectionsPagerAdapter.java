9df3cb74bf3abcff4409200dd93a51388385e2bc
Nikita Lychkovsky <nikita.lychkovsky@itechart-group.com>
2015-04-28 17:19:32 +0300

    Initial project

---
+++ PhotoMap/app/src/main/java/itechart/group/nl/photomap/ui/adapters/SectionsPagerAdapter.java
@@ -0,0 +1,46 @@
+package itechart.group.nl.photomap.ui.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.Locale;
+
+import itechart.group.nl.photomap.ui.fragments.PlaceholderFragment;
+
+/**
+ * Created by mac-229 on 4/28/15.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return "test";
+            case 1:
+                return "test2";
+            case 2:
+                return "test3";
+        }
+        return null;
+    }
+}
